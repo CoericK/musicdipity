acba7f5b36410e29ee4b1375af2710ea92c6d958
David Tran <david@upbeatpr.com>
2020-04-11 12:03:56 -0700

    First commit for Spotidipity for the Pioneer Hackathon 👋👨🏻‍💻! Let the games begin! 🥊 🛎  - Just getting familiar with the Spotipy bindings/Spotify Web API  - Search through last 50 tracks to find any overlaps using sets, assuming that if artists overlap, that's the best algo for finding a flyby!  - Some initial requirements— included Flask even though we don't have the webserver up yet. Currently, oauth works via command line  - Also add a Procfile and runtime for eventually deploying to Heroku

---
+++ server.py
@@ -0,0 +1,59 @@
+from collections import defaultdict
+import os
+import sys
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.util as util
+
+from dotenv import load_dotenv
+load_dotenv()
+
+scope = 'user-library-read,user-read-recently-played,user-read-playback-position,user-read-currently-playing,user-modify-playback-state'
+
+
+def get_auth_prompt_and_recent_tracks():
+    if len(sys.argv) > 1:
+        username = sys.argv[1]
+    else:
+        print("Usage: %s username" % (sys.argv[0],))
+        sys.exit()
+
+    token = util.prompt_for_user_token(username, scope)
+    print(token)
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        results = sp.current_user_recently_played(limit=50)
+        for item in results['items']:
+            track = item['track']
+            print(track['name'] + ' - ' + track['artists'][0]['name'])
+    else:
+        print("Can't get token for", username)
+
+
+# get_auth_prompt_and_recent_tracks()
+
+RICKY_TOKEN = os.environ['RICKY_TOKEN']
+DAVID_TOKEN = os.environ['DAVID_TOKEN']
+
+TOKENS = {
+    'rickyyean': RICKY_TOKEN,
+    'dtran320': DAVID_TOKEN,
+}
+
+user_recent_artists = {}
+
+for user, token in TOKENS.items():
+    sp = spotipy.Spotify(auth=token)
+    recent = sp.current_user_recently_played(limit=50)
+    user_recent_artists[user] = defaultdict(list)
+    for item in recent['items']:
+        user_recent_artists[user][item['track']['artists'][0]['name']].append(item['played_at'] + ': ' + item['track']['name'])
+
+overlap = set(user_recent_artists['rickyyean'].keys()) & set(user_recent_artists['dtran320'].keys())
+
+for artist in overlap:
+    print(user_recent_artists['rickyyean'][artist])
+    print(user_recent_artists['dtran320'][artist])
+
+
